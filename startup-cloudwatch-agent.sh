b98e0e7d27fbda470d50400037ad7b2a7bf7fd98
tyler.kim <writer0713@naver.com>
2022-09-05 11:46:10 +0900

    feat: cloudwatch-agent 관련 스크립트 추가
    - install, startup

---
+++ startup-cloudwatch-agent.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "
+#### Start cloudwatch-agent ####
+....
+"
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo systemctl status amazon-cloudwatch-agent
+
+echo "
+Success!!
+"
