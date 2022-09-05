2c5e86634a45ee3c05a597884298eba801e12c68
tyler.kim <writer0713@naver.com>
2022-09-05 12:48:53 +0900

    refactor: startup-cloudwatch-agent.sh 수정
    - 설정파일을 cat 하도록 추가

--- scripts/startup-cloudwatch-agent.sh
+++ scripts/startup-cloudwatch-agent.sh
@@ -5,6 +5,11 @@ echo "
 ....
 "
 
+echo "
+#### Cat config file ####
+"
+sudo cat /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 sudo systemctl status amazon-cloudwatch-agent
 
